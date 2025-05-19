da44a78e215f895d9fd6a0bae9981378d0df6e8c
pcechetto <pedrocechetto@hotmail.com>
2025-05-19 17:49:09 -0300

    fixing review service to return user

--- src/main/java/com/devsuperior/movieflix/services/ReviewService.java
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -1,7 +1,10 @@
 package com.devsuperior.movieflix.services;
 
 import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,19 +13,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final MovieRepository movieRepository;
+    private final AuthService authService;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, MovieRepository movieRepository, AuthService authService) {
         this.reviewRepository = reviewRepository;
+        this.movieRepository = movieRepository;
+        this.authService = authService;
     }
 
     @Transactional
     public ReviewDTO insert(ReviewDTO dto) {
         Review entity = new Review();
-        entity.setText(entity.getText());
-        entity.setMovie(entity.getMovie());
-        entity.setUser(entity.getUser());
+        entity.setText(dto.getText());
+
+        Movie movie = movieRepository.getReferenceById(dto.getMovieId());
+        entity.setMovie(movie);
+
+        User user = authService.authenticated();
+        entity.setUser(user);
+
         entity = reviewRepository.save(entity);
-        dto.setId(entity.getId());
-        return dto;
+        return new ReviewDTO(entity);
     }
 }
