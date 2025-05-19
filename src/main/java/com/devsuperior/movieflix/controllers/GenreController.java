92351a0043b129a3d302b896b11de4bdad8333d7
pcechetto <pedrocechetto@hotmail.com>
2025-05-19 15:36:30 -0300

    genre controller

---
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.services.GenreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/genres")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> findAll() {
+        List<GenreDTO> genres = genreService.findAll();
+        return ResponseEntity.ok().body(genres);
+    }
+}
